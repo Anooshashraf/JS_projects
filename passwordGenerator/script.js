f3023d21160ab13a0c059df6e6614fffe20764e8
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-01-22 20:56:48 +0500

    passwordGenerator

---
+++ passwordGenerator/script.js
@@ -0,0 +1,26 @@
+const passwordBox = document.getElementById("password");
+const length = 8;
+const passwordButton = document.getElementById("btn");
+const imageButton =  document.getElementById("img");
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = " 0123456789";
+const specialCharacters = "@#/$%&*(){}[]<>-~_+";
+
+const allChars = upperCase + LowerCase + number + specialCharacters;
+function GeneratePassword(){
+    let password = "";
+    password+= upperCase[Math.floor(Math.random() * upperCase.length)];
+    password+= LowerCase[Math.floor(Math.random() * LowerCase.length)];
+    password+= number[Math.floor(Math.random() * number.length)];
+    password+= specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    };
+    passwordBox.value = password;
+
+};
+
+passwordButton.addEventListener("click",GeneratePassword);
